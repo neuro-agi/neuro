9d07f10cae247ae1357c66740ce903cca0fa3dab
khushmanvar <khushmanvar@gmail.com>
2025-01-18 13:28:13 +0530

    refactor(logging): rename handler variable for clarity

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -8,9 +8,9 @@ def get_logger(name: str):
     """
     logger = logging.getLogger(name)
     if not logger.handlers:
-        handler = logging.StreamHandler()
+        stream_handler = logging.StreamHandler()
         formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
         logger.setLevel(logging.INFO)
     return logger
